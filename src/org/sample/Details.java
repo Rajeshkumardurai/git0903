d3ef8662282a616beb8f5b8fc724c9549a204080
Rajeshkumardurai <rajias20@gmail.com>
2022-03-09 18:04:27 +0530

    mytask

---
+++ src/org/sample/Details.java
@@ -0,0 +1,34 @@
+package org.sample;
+
+public class Details {
+
+	public void dept() {
+byte ece = 12;
+short block = 20;
+int building = 40;
+long staffs = 1000;
+float salary = 15000.1234f;
+double income = 20000.123351;
+char gender = 'm';
+String address = "adyar, chennai";
+boolean abovedetails = true;
+System.out.println(ece);
+System.out.println(block);
+System.out.println(building);
+System.out.println(staffs);
+System.out.println(salary);
+System.out.println(income);
+
+	}
+	public static void main(String[] args) {
+		Details d=new Details();
+		d.dept();
+		
+	}
+
+		}
+	
+	
+	
+
+
